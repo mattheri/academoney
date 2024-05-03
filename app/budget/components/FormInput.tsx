1a1dcdb77f2ea4a5b166e2f7f69a0f8154fd2e58
carocadieux <caroline-cadieux@outlook.com>
2024-05-02 20:12:54 -0400

    creation d'un composant form réutilisable, ajout de dropown catégorie

---
+++ app/budget/components/FormInput.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useState, ChangeEvent } from 'react';
+
+// Définition des types de props du composant
+interface FormInputProps {
+    label: string;
+    type: string;
+    options?: string[];
+}
+
+// Définition du composant avec ses props
+const FormInput: React.FC<FormInputProps> = ({ label, type, options }) =>  {
+    // Déclaration de l'état 
+    const [value, setValue] = useState<string>('');
+
+    // Fonction de gestion du changement de la valeur du champ
+    const onChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setValue(event.target.value);
+      };
+
+
+  // rendu du composant
+  return (
+    <div className="FormInput mb-4">
+        <label className="block p-1 text-gray-700">{label}</label>
+        {type === 'select' ? (
+            <select value={value} onChange={onChange} className="w-full mt-1 block p-1  border-gray-300 shadow-sm rounded-md focus:border-blue-500 focus:ring">
+            {options && options.map((option, index) => (
+                <option key={index} value={option}>{option}</option>
+            ))}
+            </select>
+        ) : (
+            <input
+                type={type} 
+                value={value}
+                onChange={(type === 'checkbox' || type === 'radio') ? onChange : (e) => setValue(e.target.value)}
+                className="w-full mt-1 block p-1 rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring"
+            />
+        )}
+    </div>
+  );
+};
+
+export default FormInput
---
+++ app/budget/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React from 'react';
+import FormInput from './components/FormInput';
+
+// Définition du composant BudgetPage
+const BudgetPage: React.FC = () => {
+
+  // Définition des catégories pour le champ select catégorie
+  const categories = ['Catégories',
+                      'Habitation',
+                      'Alimentation',
+                      'Télécomunications',
+                      'Éducation',
+                      'Soins personnels',
+                      'Épargne',
+                      'Vêtements',
+                      'Loisirs',
+                      'Autres'];
+
+  // Rendu du composant
+  return (
+    <div className=' bg-gray-50 flex flex-col justify-center sm:p-5 m-20 w-1/2 mx-auto '>
+      <FormInput label="Date" type="date" />
+      <FormInput label="Description" type="text" />
+      <FormInput label="" type="select" options={categories} />
+      <FormInput label="Montant" type="number" />
+    </div>
+  );
+};
+
+export default BudgetPage;
