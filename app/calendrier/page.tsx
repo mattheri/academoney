43f82f149e08e9794e9e2b9f011da5abd20fe53e
Mathieu Thériault <>
2024-05-09 08:57:01 -0400

    mathieu/ACA-66 Ajouté un peu de structure, ajouté la récupération de l'utilisateur et des events, gestion de la date actuelle par les query params, ce qui permettra de pouvoir gérer certains paramètres server side, ajouté le total par mois

--- app/calendrier/page.tsx
+++ app/calendrier/page.tsx
@@ -1,131 +1,37 @@
-"use client";
-//npm install date-fns
-import React, { ChangeEvent, useRef, useState } from 'react';
-import { format, addMonths, subMonths, addDays, startOfWeek, endOfWeek, startOfMonth, endOfMonth, eachDayOfInterval, isSameMonth } from 'date-fns';
-import { fr } from 'date-fns/locale'; 
+import { cookies } from "next/headers";
 
-interface Event {
-  date: Date;
-  name: string;
-}
+import { Transaction } from "@/auth";
+import type { Month } from "@/calendar/calendar";
+import Calendar from "@/calendar/components/Calendar";
+import appConstants from "@/contants";
+import httpClient from "@/http";
+import { PageProps } from "@/utils/types";
 
-interface CalendarProps {
-  events: Event[];
-}
+type Params = {};
 
-const Calendar: React.FC<CalendarProps> = ({ events }) => {
-  const [currentMonth, setCurrentMonth] = useState<Date>(new Date());
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null); 
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false); 
-  const [newEventName, setNewEventName] = useState<string>('');
-  const [allEvents, setAllEvents] = useState<Event[]>([]);
+type SearchParams = {
+  month?: string;
+  year?: string;
+};
 
-  const firstDayOfMonth = startOfMonth(currentMonth);
-  const lastDayOfMonth = endOfMonth(currentMonth);
-  const firstDayOfWeek = startOfWeek(firstDayOfMonth);
-  const lastDayOfWeek = endOfWeek(lastDayOfMonth);
+const CalendarPage = async ({
+  searchParams,
+}: PageProps<Params, SearchParams>) => {
+  const now = new Date();
+  const month = (Number(searchParams?.month) || now.getMonth() + 1) as Month;
+  const year = Number(searchParams?.year) || now.getFullYear();
 
-  const daysInMonth = eachDayOfInterval({
-    start: firstDayOfWeek,
-    end: lastDayOfWeek,
-  });
-
-  const handleNextMonth = () => {
-    setCurrentMonth(addMonths(currentMonth, 1));
-  };
-
-  const handlePrevMonth = () => {
-    setCurrentMonth(subMonths(currentMonth, 1));
-  };
-
-  const handleDateClick = (date: Date) => {
-    setSelectedDate(date); 
-    setIsModalOpen(true); 
-  };
-
-  const handleEventInput = (e : ChangeEvent<HTMLInputElement>) => {
-    setNewEventName(e.target.value);
-  }
-
-  const handleAddEvent = () => {
-    const newEvent: Event = {
-      date: selectedDate!,
-      name: newEventName
-    }
-
-    setAllEvents([...allEvents, newEvent]);
-    setIsModalOpen(false); 
-  }
+  const sessionCookie = cookies().get(appConstants.USER_ID_COOKIE);
+  const userId = JSON.parse(sessionCookie!.value) as number;
+  const transactions = await httpClient.GET<Transaction[]>(
+    `/users/${userId}/transactions`
+  );
 
   return (
-    <div className="mx-auto max-w-xl border p-3 bg-[#092d74] rounded-lg mt-4 text-white">
-      <div className="flex justify-between items-center mb-4">
-        <button onClick={handlePrevMonth}>&lt;</button>
-        <h1>{format(currentMonth, 'MMMM yyyy', { locale: fr })}</h1> {}
-        <button onClick={handleNextMonth}>&gt;</button>
-      </div>
-      <table className="border border-black text-center w-full rounded-lg">
-        <thead>
-          <tr>
-            {['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'].map((day) => (
-              <th key={day} className="border border-white p-2 rounded-md">{day}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {Array(Math.ceil(daysInMonth.length / 7)).fill(null).map((_, weekIndex) => (
-            <tr key={weekIndex}>
-              {Array(7).fill(null).map((_, dayIndex) => {
-                const day = addDays(firstDayOfWeek, weekIndex * 7 + dayIndex);
-                return (
-                  <td
-                    key={dayIndex}
-                    onClick={() => handleDateClick(day)}
-                    className={`border border-white p-2 ${isSameMonth(day, currentMonth) ? '' : 'other-month'}`}
-                  >
-                    {day && format(day, 'd')}
-                    <div id={day && format(day, 'yyyy-MM-dd')}>
-                        {allEvents.map((event, eventIndex) => (
-                            <p key={eventIndex}>{event.date.toString() === day.toString() ? event.name : ''}</p>
-                          ))}
-                      </div>
-                  </td>
-                );
-              })}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        <h2>Évènements:</h2>
-        <ul>
-          {allEvents?.length > 0 ? (
-            allEvents.map((event, index) => (
-              <li key={index}>
-                <strong>{format(event.date, 'dd MMMM yyyy')}</strong>: {event.name}
-              </li>
-            ))
-          ) : (
-            <li>Aucun évènements en cours</li>
-          )}
-        </ul>
-      </div>
-      {isModalOpen && (
-        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-opacity-50">
-          <div className="bg-[#092d74] p-4 rounded-md border-2 border-white">
-            <h2>Date sélectionnée: {selectedDate && format(selectedDate, 'dd MMMM yyyy', { locale: fr })}</h2>
-            <h3>Ajouter un événement</h3>
-            <form action="" className='flex flex-col'>
-                <label htmlFor="eventName">Nom:</label>
-                <input id='eventName' name='eventName' autoFocus={true} className='text-black' type="text" onChange={(e) => handleEventInput(e)} />
-            </form>
-            <button className='bg-[#bf0c1d] rounded-md p-2' onClick={() => setIsModalOpen(false)}>Annuler</button>
-            <button className='bg-[#ef3832] rounded-md p-2 ml-2 mt-2' onClick={() => handleAddEvent()}>Ajouter</button>
-          </div>
-        </div>
-      )}
-    </div>
+    <main>
+      <Calendar events={transactions.data} month={month} year={year} />
+    </main>
   );
 };
 
-export default Calendar;
+export default CalendarPage;
