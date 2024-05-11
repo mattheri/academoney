eef99f3b835c6bda18627ef219f4dc58dba92f2a
carocadieux <caroline-cadieux@outlook.com>
2024-05-11 09:26:33 -0400

    ajout du component budgetForm

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -1,34 +1,14 @@
-"use client"
 
-import { FormInput, FormSelectInput, Button, Form } from '@/common';
-import { categories, revenusDepenses } from '@/common/constants';
+import { BudgetForm } from '../budgetForm/budgetForm';
 
 // Définition du composant BudgetPage
 const BudgetPage: React.FC = () => {
   
-
-  // Fonction de soumission du formulaire
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Formulaire soumis");
-  };
-
- 
   // Rendu du composant
-  return (
-    
-      <div className=" bg-gray-500 flex flex-col justify-center sm:p-5 m-20 w-1/2 mx-auto">
-        <Form onSubmit={handleSubmit}>
-          <FormInput label="Date" type="date" />
-          <FormInput label="Description" type="text" />
-          <FormSelectInput options={categories} />
-          <FormInput label="Montant" type="number" />
-          <FormSelectInput options={revenusDepenses} />
-          <Button type="submit">Ajouter</Button>
-        </Form>
-      </div>
-    
-    
+  return (     
+    <main>
+      <BudgetForm/>
+    </main>  
   );
 };
 
---
+++ app/budgetForm/BudgetForm.tsx
@@ -0,0 +1,25 @@
+import { FormInput, FormSelectInput, Button, Form } from '@/common';
+import { categories, revenusDepenses } from '@/common/constants';
+
+// Définition du composant BudgetPage
+export const BudgetForm: React.FC = () => {
+   
+    
+    // Rendu du composant
+    return (
+       
+        <div className=" bg-gray-500 flex flex-col justify-center sm:p-5 m-20 w-1/2 mx-auto">
+            <Form>
+                <FormInput label="Date" type="date" />
+                <FormInput label="Description" type="text" />
+                <FormSelectInput options={categories} />
+                <FormInput label="Montant" type="number" />
+                <FormSelectInput options={revenusDepenses} />
+                <Button type="submit">Ajouter</Button>
+            </Form>
+        </div>
+       
+       
+    );
+};
+   
