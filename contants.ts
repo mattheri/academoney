523f0bc7e2bcbe4dcab4ffd5a2dd79153fff10cf
Mathieu Theriault <mathieu.theriault89@gmail.com>
2024-05-21 20:50:12 -0400

    Fix errors with http service

--- contants.ts
+++ contants.ts
@@ -1,6 +1,8 @@
 const appConstants = {
   API_URL: process.env.API_URL || "http://localhost:3000",
   USER_ID_COOKIE: "session",
+  PASSTHROUGH_SECRET: process.env.PASSTHROUGH_SECRET,
+  PASSTHROUGH_QUERY_PARAM: "passthrough",
 };
 
 export default appConstants;
